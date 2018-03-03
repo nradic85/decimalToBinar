d3f14ca5c46e3a6f23f5413a184a6cdd745efa98
Nikola Radić <nradic85@gmail.com>
2018-03-03 12:20:35 +0100

    dodavanje stranice za preracunjanjave

---
+++ decimalToBinar/decimalToBinar/DecimalToBinar.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DecimalToBinar.aspx.cs" Inherits="decimalToBinar.DecimalToBinar" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Label runat="server" Text="Insert decimal number:"></asp:Label>
+    <asp:TextBox id="decimalNumber" runat="server" />
+    <asp:Button runat="server" OnClick="convert" Text="Convert number" />
+    <asp:Label runat="server" ID="binarNumber" Text=""></asp:Label>
+</asp:Content>
---
+++ decimalToBinar/decimalToBinar/DecimalToBinar.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace decimalToBinar
+{
+    public partial class DecimalToBinar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void convert(object sender, EventArgs e)
+        {
+            int value = Int32.Parse(decimalNumber.Text);
+            string binary = Convert.ToString(value, 2);
+            binarNumber.Text = "Binarna vrijednost broja "+value+" je: "+binary;
+        }
+    }
+}
