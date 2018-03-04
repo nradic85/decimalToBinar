afa86b2e4f57b546bb541737bc00be5ac18dda34
Nikola Radić <nradic85@gmail.com>
2018-03-04 14:54:31 +0100

    Podešeno da radi na svim brojevnim sustavima

--- decimalToBinar/decimalToBinar/DecimalToBinar.aspx.cs
+++ decimalToBinar/decimalToBinar/DecimalToBinar.aspx.cs
@@ -16,9 +16,45 @@ namespace decimalToBinar
 
         protected void convert(object sender, EventArgs e)
         {
-            int value = Int32.Parse(decimalNumber.Text);
-            string binary = Convert.ToString(value, 2);
-            binarNumber.Text = "Binarna vrijednost broja "+value+" je: "+binary;
+            //int value = Int32.Parse(decimalNumber.Text);
+            //string binary = Convert.ToString(value, 2);
+            //binarNumber.Text = "Binarna vrijednost broja "+value+" je: "+binary;
+            int i = 0;
+            string[] brojevi = { "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G"};
+            int j = 0;
+            int bs = 16; //brojevni sustav
+            int dn = 0;
+            try
+            {
+                dn = Int32.Parse(decimalNumber.Text);
+                int temp_dn = dn;
+                if (temp_dn != 0)
+                {
+                    while (temp_dn > 0)
+                    {
+                        temp_dn = temp_dn / bs;
+                        j++;
+                    }
+                }
+                else
+                {
+                    j = 1;
+                }
+                string[] tempList = new string[j]; //deklariranje liste
+                while (dn > 0)
+                {
+                    i = dn % bs;
+                    tempList[j - 1] = brojevi[i];
+                    dn = dn - i;
+                    dn = dn / bs;
+                    j--;
+                }
+                binarNumber.Text = string.Join("", tempList);
+            }
+            catch
+            {
+                binarNumber.Text = "Unesena vrijednost nije iz dekatskog sustava brijeva!!!!!";
+            }
         }
     }
 }
